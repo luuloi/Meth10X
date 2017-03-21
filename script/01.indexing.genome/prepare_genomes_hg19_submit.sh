96a9e2589c5943efd744afb8aa2bce7306aa71bc
luuloi <luu.p.loi@googlemail.com>
2017-03-21 18:40:18 +1100

    delete all

--- script/01.indexing.genome/prepare_genomes_hg19_submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-o='/home/phuluu/data/annotations/hg19/'
-e='/home/phuluu/data/annotations/hg19/'
-
-qsub -j y -pe smp 20 -l h_vmem=2000G -wd `pwd` -b y -o $o -e $e bash "/home/phuluu/Projects/WGBS10X_new/V02/script/genome.hg19/prepare_genomes_hg19.sh"
-
-
-
